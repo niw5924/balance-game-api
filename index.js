76dc6d40f72e846bc68783d551b7b93f336e5a1c
niw5924 <niw5924@naver.com>
2025-04-01 08:42:43 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+﻿const express = require('express');
+const app = express();
+const port = 3000;
+
+// 라우터 불러오기
+const questionsRouter = require('./routes/questions');
+const typesRouter = require('./routes/types');
+
+app.use('/api/questions', questionsRouter);
+app.use('/api/types', typesRouter);
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
---
+++ routes/types.js
@@ -0,0 +1,17 @@
+﻿const express = require('express');
+const router = express.Router();
+
+// 유형 데이터
+const types = [
+    "감성형", "이성형", "모험형", "회피형", "자기중심형",
+    "공감형", "돌직구형", "이상주의형", "현실주의형", "낭만형",
+    "도전형", "관찰자형", "쾌락추구형", "신중형", "충동형",
+    "자기희생형", "호기심형", "분석형", "기피형", "전통중시형"
+];
+
+// GET /api/types
+router.get('/', (req, res) => {
+    res.json(types);
+});
+
+module.exports = router;
